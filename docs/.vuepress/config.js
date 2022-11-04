e368a425df882537985d1a147a62d8bdae155bc1
mirza <mirza.akhena@gmail.com>
2022-11-04 09:23:10 +0700

    set base to gogen

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
+  base: '/gogen/',
   lang: 'en-US',
   title: 'Hello VuePress',
   description: 'Just playing around',
