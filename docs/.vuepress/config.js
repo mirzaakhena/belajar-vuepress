32032ad3561bba2108f35dda730d3602cb6c1e23
mirza <mirza.akhena@gmail.com>
2022-11-04 09:38:16 +0700

    rollback to gogen

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
-  base: '/belajar-vuepress/',
+  base: '/gogen/',
   lang: 'en-US',
   title: 'Hello VuePress',
   description: 'Just playing around',
